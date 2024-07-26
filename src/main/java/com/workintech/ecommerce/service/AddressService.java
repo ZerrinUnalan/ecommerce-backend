c179adc14412c66920cc4a3348b060f03f783d42
Zerrin Unalan <zerunalan@gmail.com>
2024-07-26 22:45:42 +0300

    Updated AddressService

--- src/main/java/com/workintech/ecommerce/service/AddressService.java
+++ src/main/java/com/workintech/ecommerce/service/AddressService.java
@@ -10,3 +10,24 @@ public interface AddressService {
     List<Address> getAllAddress();
     String saveAddress(Address address);
 }
+
+// package com.workintech.ecommerce.service;
+//
+//import com.workintech.ecommerce.dto.AddressResponseDto;
+//import com.workintech.ecommerce.entity.Address;
+//import com.workintech.ecommerce.mapper.AddressMapper;
+//import com.workintech.ecommerce.repository.AddressRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class AddressService {
+//
+//    @Autowired
+//    private AddressRepository addressRepository;
+//
+//    public AddressResponseDto getAddressById(Long id) {
+//        Address address = addressRepository.findById(id).orElseThrow(() -> new RuntimeException("Address not found"));
+//        return AddressMapper.INSTANCE.addressToAddressResponseDto(address);
+//    }
+//}
